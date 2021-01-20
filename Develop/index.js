ab26ae00e7f75ae52f1726cec5ba58dbf6d5ce28
Greg Scott <gtscott90@gmail.com>
2021-01-19 23:16:35 -0500

    coded the prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,44 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "Enter a brief description of your project.",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter any specific installation instructions.",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "Enter any usage of project.",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Enter any licensing informtion.",
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "Enter any contribution guidlines.",
+            name: "contribution",
+        },
+        {
+            type: "input",
+            message: "Enter any test instructions.",
+            name: "tests",
+        },
+    ])
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
